39f34495bf9fdf764088180c94ad7e7adc992b39
alondracv01 <alondra.carrasco@uabc.edu.mx>
2023-03-10 21:19:51 -0800

    Se agregaron unas cosillas

--- app/src/main/java/com/hfad/asesoriasuabc/LogInActivity.java
+++ app/src/main/java/com/hfad/asesoriasuabc/LogInActivity.java
@@ -1,4 +1,23 @@
 package com.hfad.asesoriasuabc;
 
-public class LogInActivity {
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LogInActivity extends AppCompatActivity {
+
+    Button ingresar;
+    EditText usuarioEdit;
+    EditText contrasenaEdit;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        usuarioEdit = (EditText) findViewById(R.id.usuario_editText);
+        contrasenaEdit = (EditText) findViewById(R.id.contrasena_editText);
+        ingresar = (Button) findViewById(R.id.ingresar_button);
+    }
 }
--- app/src/main/java/com/hfad/asesoriasuabc/MainActivity.java
+++ app/src/main/java/com/hfad/asesoriasuabc/MainActivity.java
@@ -1,14 +1,173 @@
 package com.hfad.asesoriasuabc;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private int actualFrag = R.id.nav_materias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Fragment fragment = null;
+
+        Boolean isFirstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE)
+                .getBoolean("isFirstRun", true);
+
+        if (isFirstRun) {
+            startActivity(new Intent(MainActivity.this, LogInActivity.class));
+        }
+
+        getSharedPreferences("PREFERENCE", MODE_PRIVATE).edit().putBoolean("isFirstRun", false).commit();
+
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                drawer,
+                toolbar,
+                R.string.nav_open_drawer,
+                R.string.nav_close_drawer);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        if (savedInstanceState != null){
+            fragment = new MateriasFragment();
+            switch (savedInstanceState.getInt("fragmento")){
+                case R.id.nav_citas:
+                    fragment = new CitasFragment();
+                    actualFrag = R.id.nav_citas;
+                    break;
+                case R.id.nav_asesores:
+                    fragment = new AsesoresFragment();
+                    actualFrag = R.id.nav_asesores;
+                    break;
+                case R.id.nav_solicitar:
+                    fragment = new SolicitarFragment();
+                    actualFrag = R.id.nav_solicitar;
+                    break;
+                default:
+                    fragment = new MateriasFragment();
+                    actualFrag = R.id.nav_materias;
+                    break;
+            }
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame, fragment);
+            ft.commit();
+        } else {
+            fragment = new MateriasFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.content_frame, fragment);
+            ft.commit();
+        }
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Fragment fragment = null;
+        switch (actualFrag){
+            case R.id.nav_citas:
+                fragment = new CitasFragment();
+                actualFrag = R.id.nav_citas;
+                break;
+            case R.id.nav_asesores:
+                fragment = new AsesoresFragment();
+                actualFrag = R.id.nav_asesores;
+                break;
+            case R.id.nav_solicitar:
+                fragment = new SolicitarFragment();
+                actualFrag = R.id.nav_asesores;
+                break;
+            default:
+                fragment = new MateriasFragment();
+                actualFrag = R.id.nav_materias;
+                break;
+        }
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.content_frame, fragment);
+        ft.commit();
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        Fragment fragment = null;
+        Intent intent = null;
+        switch (id){
+            case R.id.nav_citas:
+                fragment = new CitasFragment();
+                actualFrag = R.id.nav_citas;
+                break;
+            case R.id.nav_asesores:
+                fragment = new AsesoresFragment();
+                actualFrag = R.id.nav_asesores;
+                break;
+            case R.id.nav_solicitar:
+                fragment = new SolicitarFragment();
+                actualFrag = R.id.nav_asesores;
+                break;
+            default:
+                fragment = new MateriasFragment();
+                actualFrag = R.id.nav_materias;
+                break;
+        }
+
+        if (fragment != null){
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame, fragment);
+            ft.commit();
+        } else {
+            startActivity(intent);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed(){
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("fragmento", actualFrag);
     }
 }
